aa80f981e74dd5daa82a3a963ff265a997c7e8dc
sergey.kislinskiy <sergey.kislinskiy@strikersoft.com>
2016-02-12 22:06:06 +0200

    project created

---
+++ TwitterProxy/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TwitterProxy.Models;
+
+namespace TwitterProxy.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult History()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Twitter(TwitterRequest model)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ TwitterProxy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TwitterProxy.Startup))]
+namespace TwitterProxy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
